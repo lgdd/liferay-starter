2e53093e0b8c151d734547b3d99b1c20e88570a2
lgdd <louis-guillaume.durand@liferay.com>
2020-04-20 08:39:40 +0200

    :goal_net: Add params validation with proper response

--- src/main/java/com/github/lgdd/liferay/starter/LiferayStarterResource.java
+++ src/main/java/com/github/lgdd/liferay/starter/LiferayStarterResource.java
@@ -13,6 +13,8 @@ import javax.ws.rs.core.Response;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 import static javax.ws.rs.core.Response.ok;
 
@@ -31,21 +33,35 @@ public class LiferayStarterResource {
                               @QueryParam("projectGroupId") String projectGroupId,
                               @QueryParam("projectArtifactId") String projectArtifactId,
                               @QueryParam("projectVersion") String projectVersion) {
-        try {
-            byte[] workspaceZip = workspaceCreator
-                    .createWorkspaceZip(tool, version, projectGroupId, projectArtifactId, projectVersion);
+        if(validateWorkspaceParams(tool, version, projectArtifactId)) {
+            try {
+                byte[] workspaceZip = workspaceCreator
+                        .createWorkspaceZip(tool, version, projectGroupId, projectArtifactId, projectVersion);
 
-            String filename = projectArtifactId.isEmpty()?
-                    workspaceCreator.getWorkspaceName(tool, version) : projectArtifactId;
+                String filename = projectArtifactId.isEmpty()?
+                        workspaceCreator.getWorkspaceName(tool, version) : projectArtifactId;
 
-            Response.ResponseBuilder responseBuilder = Response.ok(workspaceZip);
-            responseBuilder.type("application/zip");
-            responseBuilder.header("Content-disposition", "attachment; filename=" + filename + ".zip");
-            return responseBuilder.build();
-        } catch (Exception e) {
-            log.error(e.getLocalizedMessage(), e);
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+                Response.ResponseBuilder responseBuilder = Response.ok(workspaceZip);
+                responseBuilder.type("application/zip");
+                responseBuilder.header("Content-disposition", "attachment; filename=" + filename + ".zip");
+                return responseBuilder.build();
+            } catch (Exception e) {
+                log.error(e.getLocalizedMessage(), e);
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+            }
         }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    private boolean validateWorkspaceParams(String tool, String version, String projectArtifactId) {
+        List<String> liferayVersions = Arrays.asList("7.3", "7.2", "7.1", "7.0");
+        if (!("gradle".equalsIgnoreCase(tool) || "maven".equalsIgnoreCase(tool))) {
+            return false;
+        }
+        if(!liferayVersions.contains(version)) {
+            return false;
+        }
+        return projectArtifactId != null;
     }
 
 }
